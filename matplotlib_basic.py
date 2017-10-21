70af68e17d06c43bc3840e307673c05eb17fbd07
hongjun423 <hongjc423@naver.com>
2017-10-21 13:43:59 +0900

    pyplot 그래프 그리기
    matplotlib의 pyplot이용하여 그래프 그리기

--- matplotlib_basic.py
+++ matplotlib_basic.py
@@ -9,7 +9,16 @@ print(x)
   3.   3.1  3.2  3.3  3.4  3.5  3.6  3.7  3.8  3.9  4.   4.1  4.2  4.3  4.4
   4.5  4.6  4.7  4.8  4.9  5.   5.1  5.2  5.3  5.4  5.5  5.6  5.7  5.8  5.9]
 """
-y = np.sin(x)
+y1 = np.sin(x)
 
-plt.plot(x, y)#그래프 생성
+plt.plot(x, y1)#그래프 생성
 plt.show()#그래프 출력
+
+y2 = np.cos(x)
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle="--", label="cos")#점선으로 그리기
+plt.xlabel("X")
+plt.ylabel("y")
+plt.title('sin & cos')#제목 표시
+plt.legend()#legend 표시
+plt.show()
